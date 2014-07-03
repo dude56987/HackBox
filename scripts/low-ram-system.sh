9297da3c8bf6185f327afdf1d22b38f2b0536512
dude56987 <dude56987@users.noreply.github.com>
2014-07-03 12:33:42 -0400

    Changed low ram requirement to below one gig

--- scripts/low-ram-system.sh
+++ scripts/low-ram-system.sh
@@ -3,10 +3,11 @@
 memory=$(cat /proc/meminfo | grep MemTotal | sed "s/\ /\n/g" | grep [0987654321])
 echo "$memory KB of total memory in the system..."
 # memory is showm in kilobytes, current default is below 2 gigs
-if [ "$memory" -lt 2000000 ];then
+if [ "$memory" -lt 1000000 ];then
 	echo 'Setting up system for low ram pc...'
 	apt-get install fluxbox --assume-yes
 	apt-get install nitrogen --assume-yes
+	apt-get install midori --assume-yes
 	sed -i "s/Session=*\n/Session=fluxbox\n/g" /etc/skel/.dmrc
 	for dir in /home/*;do
 		echo "Editing $dir/.dmrc ..."
