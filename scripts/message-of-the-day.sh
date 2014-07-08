eff44a6abdc0caf4eabf4a87eff0f97eeea8e3ef
dude56987 <dude56987@users.noreply.github.com>
2014-07-07 20:11:56 -0400

    Added a message of the day script

---
+++ scripts/message-of-the-day.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# add message of the day script to system startup
+sed -i "s/exit 0//g" /etc/rc.local
+sed -i "s/message-of-the-day//g" /etc/rc.local
+echo 'message-of-the-day' >> /etc/rc.local
+echo 'exit 0' >> /etc/rc.local
+# write the message of the day script
+echo "#! /bin/bash" > /usr/bin/message-of-the-day
+echo 'for dir in /home/*;do' >> /usr/bin/message-of-the-day
+echo "echo \"Editing \$dir/.motd ...\"" >> /usr/bin/message-of-the-day
+echo "fortune -a > \$dir/.motd" >> /usr/bin/message-of-the-day
+echo "chown \$(echo \$dir | sed \"s/\/home\///g\") \$dir/.motd" >> /usr/bin/message-of-the-day
+echo 'done' >> /usr/bin/message-of-the-day
+chown root /usr/bin/message-of-the-day
+chmod u+xr /usr/bin/message-of-the-day
+chmod go-wx /usr/bin/message-of-the-day
+chmod go+r /usr/bin/message-of-the-day
