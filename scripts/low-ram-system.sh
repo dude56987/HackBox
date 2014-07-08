6a1ff5aa83e97bc2c04c4976ad716f0a5e52ca3a
dude56987 <dude56987@users.noreply.github.com>
2014-07-07 21:43:11 -0400

    Created FluxBox custom theme

--- scripts/low-ram-system.sh
+++ scripts/low-ram-system.sh
@@ -2,12 +2,14 @@
 # Check ram to dertermine if it is a low ram system
 memory=$(cat /proc/meminfo | grep MemTotal | sed "s/\ /\n/g" | grep [0987654321])
 echo "$memory KB of total memory in the system..."
-# memory is showm in kilobytes, current default is below 2 gigs
-if [ "$memory" -lt 1000000 ];then
+# memory is showm in kilobytes, current default is below 1.6 gigs
+if [ "$memory" -lt 1600000 ];then
 	echo 'Setting up system for low ram pc...'
 	apt-get install fluxbox --assume-yes
 	apt-get install nitrogen --assume-yes
 	apt-get install midori --assume-yes
+	# copy over the style for fluxbox to use
+	cp /opt/hackbox/media/themes/HackBox_Fluxbox /usr/share/fluxbox/styles/
 	#modify the configs in /etc/skel
 	sed -i "s/Session=*\n/Session=fluxbox\n/g" /etc/skel/.dmrc
 	sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"fluxbox\"/g" /etc/skel/.xinitrc
