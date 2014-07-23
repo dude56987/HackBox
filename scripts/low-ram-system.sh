6f91cac79ee9732df65ee746011dbc4d953da9fa
dude56987 <dude56987@users.noreply.github.com>
2014-07-23 16:21:51 -0400

    Removed fluxbox configs,changed low ram wm to LXDE

--- scripts/low-ram-system.sh
+++ scripts/low-ram-system.sh
@@ -5,21 +5,18 @@ echo "$memory KB of total memory in the system..."
 # memory is showm in kilobytes, current default is below 1.6 gigs
 if [ "$memory" -lt 1600000 ];then
 	echo 'Setting up system for low ram pc...'
-	apt-get install fluxbox --assume-yes
-	apt-get install nitrogen --assume-yes
+	apt-get install lxde --assume-yes
 	apt-get install midori --assume-yes
-	# copy over the style for fluxbox to use
-	cp /opt/hackbox/media/themes/HackBox_Fluxbox /usr/share/fluxbox/styles/
 	#modify the configs in /etc/skel
-	sed -i "s/Session=*\n/Session=fluxbox\n/g" /etc/skel/.dmrc
-	sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"fluxbox\"/g" /etc/skel/.xinitrc
+	sed -i "s/Session=*\n/Session=LXDE\n/g" /etc/skel/.dmrc
+	sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"LXDE\"/g" /etc/skel/.xinitrc
 	# modify existing users
 	for dir in /home/*;do
 		echo "Editing $dir/.dmrc ..."
-		sed -i "s/Session=*\n/Session=fluxbox\n/g" $dir/.dmrc
+		sed -i "s/Session=*\n/Session=LXDE\n/g" $dir/.dmrc
 		chown $(echo \$dir | sed \"s/\/home\///g\") $dir/.dmrc
 		# for antix debian compatibility
-		sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"fluxbox\"/g" $dir/.xinitrc
+		sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"LXDE\"/g" $dir/.xinitrc
 		chown $(echo \$dir | sed \"s/\/home\///g\") $dir/.xinitrc
 	done
 fi
