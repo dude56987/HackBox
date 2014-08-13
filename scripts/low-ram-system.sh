d46b0e92462d3ee84a66d303eaae3df11a033103
dude56987 <dude56987@users.noreply.github.com>
2014-08-13 01:44:37 -0400

    Further improvements to tty logins

--- scripts/low-ram-system.sh
+++ scripts/low-ram-system.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+##############################################
+# A script to setup login for a low ram system
+##############################################
 # Check ram to dertermine if it is a low ram system
 memory=$(cat /proc/meminfo | grep MemTotal | sed "s/\ /\n/g" | grep [0987654321])
 echo "$memory KB of total memory in the system..."
@@ -16,14 +19,19 @@ if [ "$memory" -lt 1600000 ];then
 	apt-get purge kdm --assume-yes
 	#modify the configs in /etc/skel
 	sed -i "s/Session=*\n/Session=LXDE\n/g" /etc/skel/.dmrc
-	sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"LXDE\"/g" /etc/skel/.xinitrc
+	echo 'lxsession' > /etc/skel/.xinitrc
+	# edit the default extra groups users are added to when new users are made
+	echo 'EXTRA_GROUPS="audio"' >> /etc/useradd.conf
+	echo 'ADD_EXTRA_GROUPS=1' >> /etc/useradd.conf
 	# modify existing users
 	for dir in /home/*;do
 		echo "Editing $dir/.dmrc ..."
 		sed -i "s/Session=*\n/Session=LXDE\n/g" $dir/.dmrc
 		chown $(echo \$dir | sed \"s/\/home\///g\") $dir/.dmrc
 		# for antix debian compatibility
-		sed -i "s/DEFAULT_DESKTOP=\".*\"/DEFAULT_DESKTOP=\"LXDE\"/g" $dir/.xinitrc
+		echo 'lxsession' > $dir/.xinitrc
 		chown $(echo \$dir | sed \"s/\/home\///g\") $dir/.xinitrc
+		# give all users permissions for the audio group
+		usermod -a -G audio $(echo \$dir | sed \"s/\/home\///g\")
 	done
 fi
---
+++ scripts/power-control-logins.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+#######################################################################
+# A script to create a shutdown and reboot user. These users require no 
+# password and only peform what they are called when used without a 
+# interactive shell.
+#######################################################################
+# create shutdown user home directories
+mkdir -p /etc/power-control-logins
+mkdir -p /etc/power-control-logins/shutdown
+mkdir -p /etc/power-control-logins/reboot
+# create bash rc files to lauch on login for the power-control login accounts
+echo "sudo shutdown -P 'now'" > /etc/power-control-logins/shutdown/.bash_login
+echo "sudo reboot" > /etc/power-control-logins/reboot/.bash_login
+# create a copy of the /etc/sudoers file for editing
+cp /etc/sudoers /tmp/sudoers.tmp
+# edit the sudoers file, if it has not already been edited
+if ! more /tmp/sudoers.tmp | grep "shutdown ALL=(ALL) NOPASSWD:/sbin/shutdown"; then
+	echo "shutdown ALL=(ALL) NOPASSWD:/sbin/shutdown" >> /tmp/sudoers.tmp
+fi
+if ! more /tmp/sudoers.tmp | grep "reboot ALL=(ALL) NOPASSWD:/sbin/reboot"; then
+	echo "reboot ALL=(ALL) NOPASSWD:/sbin/reboot" >> /tmp/sudoers.tmp
+fi
+# check if changes to file are valid
+if visudo -c -f /tmp/sudoers.tmp; then
+	# copy over the edits to the sudoers file
+	cp -v /tmp/sudoers.tmp /etc/sudoers
+else
+	echo '/etc/sudoers file edits failed!'
+fi
+# delete user if they exist to remake them
+deluser shutdown
+deluser reboot
+# switch to useradd command as it requires less crap
+useradd --system -M --no-user-group --home /etc/power-control-logins/shutdown/ --shell /bin/bash --password password shutdown
+useradd --system -M --no-user-group --home /etc/power-control-logins/reboot/ --shell /bin/bash --password password reboot
+# delete password and enable login for passwordless login to allow shutdown and reboot
+passwd -d -u shutdown
+passwd -d -u reboot
