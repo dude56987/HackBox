dc210939817b125de4788a40d1526d45f36415f3
dude56987 <dude56987@users.noreply.github.com>
2014-08-16 00:37:39 -0400

    Improvements to power-control-logins script

--- scripts/power-control-logins.sh
+++ scripts/power-control-logins.sh
@@ -9,8 +9,12 @@ mkdir -p /etc/power-control-logins
 mkdir -p /etc/power-control-logins/shutdown
 mkdir -p /etc/power-control-logins/reboot
 # create bash rc files to lauch on login for the power-control login accounts
-echo "sudo shutdown -P 'now' &while true; do read;done" > /etc/power-control-logins/shutdown/.bash_login
-echo "sudo reboot &while true; do read;done" > /etc/power-control-logins/reboot/.bash_login
+echo "sudo shutdown -P 'now' &exit" > /etc/power-control-logins/shutdown/.bash_login
+echo "sudo shutdown -P 'now' &exit" > /etc/power-control-logins/shutdown/.xinitrc
+echo "sudo shutdown -P 'now' &exit" > /etc/power-control-logins/shutdown/.bashrc
+echo "sudo reboot &exit" > /etc/power-control-logins/reboot/.bash_login
+echo "sudo reboot &exit" > /etc/power-control-logins/reboot/.xinitrc
+echo "sudo reboot &exit" > /etc/power-control-logins/reboot/.bashrc
 # create a copy of the /etc/sudoers file for editing
 cp /etc/sudoers /tmp/sudoers.tmp
 # edit the sudoers file, if it has not already been edited
@@ -19,6 +23,12 @@ if ! more /tmp/sudoers.tmp | grep "shutdown ALL=(ALL) NOPASSWD:/sbin/shutdown"; 
 fi
 if ! more /tmp/sudoers.tmp | grep "reboot ALL=(ALL) NOPASSWD:/sbin/reboot"; then
 	echo "reboot ALL=(ALL) NOPASSWD:/sbin/reboot" >> /tmp/sudoers.tmp
+fi
+if ! more /tmp/sudoers.tmp | grep "power-control ALL=(ALL) NOPASSWD:/sbin/reboot"; then
+	echo "power-control ALL=(ALL) NOPASSWD:/sbin/reboot" >> /tmp/sudoers.tmp
+fi
+if ! more /tmp/sudoers.tmp | grep "power-control ALL=(ALL) NOPASSWD:/sbin/shutdown"; then
+	echo "power-control ALL=(ALL) NOPASSWD:/sbin/shutdown" >> /tmp/sudoers.tmp
 fi
 # check if changes to file are valid
 if visudo -c -f /tmp/sudoers.tmp; then
@@ -30,9 +40,13 @@ fi
 # delete user if they exist to remake them
 deluser shutdown
 deluser reboot
-# switch to useradd command as it requires less crap
+# add special users and groups
 useradd --system -M --no-user-group --home /etc/power-control-logins/shutdown/ --shell /bin/bash --password password shutdown
 useradd --system -M --no-user-group --home /etc/power-control-logins/reboot/ --shell /bin/bash --password password reboot
+# add group for giving users power controls
+# (ADD ANY USERS YOU WANT TO HAVE SHUTDOWN AND REBOOT PERMISSIONS TO THIS GROUP)
+addgroup --system power-control
 # delete password and enable login for passwordless login to allow shutdown and reboot
+# (DISABLE THE BELOW TO REQUIRE PASSWORDED SHUTDOWN AND REBOOT, YOU WILL HAVE TO SET THE PASSWORDS)
 passwd -d -u shutdown
 passwd -d -u reboot
