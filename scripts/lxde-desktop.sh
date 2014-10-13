eb6e3a14cf4e79780ab4dad165013aafad120b3e
dude56987 <dude56987@users.noreply.github.com>
2014-10-13 01:17:58 -0400

    Moved chvt command scripting to lxde script

--- scripts/lxde-desktop.sh
+++ scripts/lxde-desktop.sh
@@ -16,6 +16,13 @@ apt-get purge gdm --assume-yes
 apt-get purge lightdm --assume-yes
 apt-get purge wdm --assume-yes
 apt-get purge kdm --assume-yes
+# change the virtual terminal to tty2 on boot
+# this is because some systems output to tty1
+# and default to it for the users input
+sed -i "s/exit 0//g" /etc/rc.local
+sed -i "s/chvt 2//g" /etc/rc.local
+echo 'chvt 2' >> /etc/rc.local
+echo 'exit 0' >> /etc/rc.local
 #modify the configs in /etc/skel
 sed -i "s/Session=*\n/Session=LXDE\n/g" /etc/skel/.dmrc
 echo 'lxsession' > /etc/skel/.xinitrc
