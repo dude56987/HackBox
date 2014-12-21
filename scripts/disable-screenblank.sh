e9206d29cf0128725295fca199bc9ed90b114b2b
dude56987 <dude56987@users.noreply.github.com>
2014-12-20 20:52:27 -0500

    Disable-bootsplash script made more percise

--- scripts/disable-screenblank.sh
+++ scripts/disable-screenblank.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# disable the boot splashscreen to improve boot speed
+# disable screenblanking in tty
 if more /etc/default/grub | grep "#GRUB_CMDLINE_LINUX_DEFAULT";then
 	sudo sed -i -e 's/#GRUB_CMDLINE_LINUX_DEFAULT/GRUB_CMDLINE_LINUX_DEFAULT/g' /etc/default/grub
 	while more /etc/default/grub | grep "##";do
@@ -7,9 +7,9 @@ if more /etc/default/grub | grep "#GRUB_CMDLINE_LINUX_DEFAULT";then
 	done
 	sudo update-grub
 fi
-if more /etc/default/grub | grep "GRUB_CMDLINE_LINUX_DEFAULT=.*splash";then
+if ! more /etc/default/grub | grep "consoleblank=0";then
 	# change line of boot to disable console blanking
-	sudo sed -i -e 's/^GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT="consoleblank=0"/g' /etc/default/grub
+	sudo sed -i -e 's/^GRUB_CMDLINE_LINUX_DEFAULT.*$/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash consoleblank=0"/g' /etc/default/grub
 	# remove blanklines
 	sudo sed -i '/^$/d' /etc/default/grub
 	# update grub config
