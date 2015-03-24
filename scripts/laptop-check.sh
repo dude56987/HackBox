611f8add081cc12359081473726c4da4c9a7cae1
dude56987 <dude56987@users.noreply.github.com>
2015-03-24 03:18:41 -0400

    Added a GPL header to laptop-check script

--- scripts/laptop-check.sh
+++ scripts/laptop-check.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
+########################################################################
+# Detect if system is a laptop and setup laptop settings
+# Copyright (C) 2015  Carl J Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
 # check if its a laptop
 if laptop-detect;then
 	# install laptop-mode-tools package
