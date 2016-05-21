a950d9d8da2d6b41077ba559830096ebd9b85719
dude56987 <dude56987@users.noreply.github.com>
2016-05-20 20:57:24 -0400

    Improvements to laptop-check script
    - Install laptop-detect if it is not found on the system
    - Added some more output while the script is running

--- scripts/laptop-check.sh
+++ scripts/laptop-check.sh
@@ -16,8 +16,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ########################################################################
-# check if the system running this script is a laptop
+echo "Running laptop check script..."
+if which laptop-detect;then
+	echo "laptop-detect found on system :D"	
+else
+	echo "Installing laptop-detect..."
+	sudo apt-get install laptop-detect --assume-yes
+fi
+echo "Checking if the running system is a laptop..."
 if laptop-detect;then
+	echo "System is a laptop, configuring system..."
 	# install tlp (improves battery life in laptops)
 	sudo apt-get install tlp --assume-yes
 	# start tlp
@@ -28,4 +36,6 @@ if laptop-detect;then
 	sudo apt-get install powertop --assume-yes
 	# create a powertop autotune script to run on boot
 	echo "$(which powertop) --auto-tune" > /etc/rc.local.d/powertop
+else
+	echo "Running system is not a laptop. End of program."
 fi
